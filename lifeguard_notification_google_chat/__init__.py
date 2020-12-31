706792c77bd9598ddf16ff8b10ee38928208b044
Diego Rubin <contact@diegorubin.dev>
2020-12-31 10:53:14 -0300

    bumps version

--- lifeguard_notification_google_chat/__init__.py
+++ lifeguard_notification_google_chat/__init__.py
@@ -1,7 +1,6 @@
 """
 Lifeguard integration with Google Chat
 """
-from lifeguard_notification_google_chat.notifications import \
-    GoogleNotificationBase
+from lifeguard_notification_google_chat.notifications import GoogleNotificationBase
 
-VERSION = "0.0.1"
+VERSION = "0.0.2"
